3ab944a97711acb216a1fc7750ea255a046dfa6c
TheWiseSailor <rudasilljay@gmail.com>
2023-12-07 13:41:15 -0500

    Basic place holder images and a click for more button

--- src/components/PropertyHomePage.js
+++ src/components/PropertyHomePage.js
@@ -1,60 +1,62 @@
 import React from "react";
+import Listing1Image from "../components/images/PropertyHomepageImages/Listing1.png";
+import Listing2Image from "../components/images/PropertyHomepageImages/Listing2.png";
+import Listing3Image from "../components/images/PropertyHomepageImages/Listing3.png";
+import Listing4Image from "../components/images/PropertyHomepageImages/Listing4.png";
+import Listing5Image from "../components/images/PropertyHomepageImages/Listing5.png";
+
 import "../App.css";
 
 const PropertyHomePage = () => {
   const topProperties = [
     {
       id: 1,
-      image:
-        "https://images.unsplash.com/photo-1600585154526-990dced4db0d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      price: "$500,000",
-      details: "3 beds / 2 baths / 1500 sqft",
-      address: "123 Main St",
+      image: Listing1Image,
+      price: "$1,200,000 / $12-20k Month",
+      details: "3 beds / 2 baths / 3500 sqft",
+      address: "Wilmington, NC",
     },
     {
       id: 2,
-      image:
-        "https://images.unsplash.com/photo-1600585154526-990dced4db0d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      price: "$700,000",
-      details: "4 beds / 3 baths / 2000 sqft",
-      address: "456 Oak St",
+      image: Listing2Image,
+      price: "$700,000 / $7-14k Month",
+      details: "2 beds / 3 baths / 2000 sqft",
+      address: "Charleston, SC",
     },
     {
       id: 3,
-      image:
-        "https://images.unsplash.com/photo-1600585154526-990dced4db0d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      price: "$600,000",
+      image: Listing3Image,
+      price: "$600,000 / $6-12k Month",
       details: "3 beds / 2.5 baths / 1800 sqft",
-      address: "789 Pine St",
+      address: "Wilmington, NC",
     },
     {
       id: 4,
-      image:
-        "https://images.unsplash.com/photo-1600585154526-990dced4db0d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      price: "$800,000",
+      image: Listing4Image,
+      price: "$800,000 / $8-16k Month",
       details: "5 beds / 4 baths / 2500 sqft",
-      address: "101 Elm St",
+      address: "Wrightsville Beach, NC",
     },
     {
       id: 5,
-      image:
-        "https://images.unsplash.com/photo-1600585154526-990dced4db0d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      price: "$900,000",
+      image: Listing5Image,
+      price: "$900,000 / $9-18k Month",
       details: "6 beds / 3.5 baths / 3000 sqft",
-      address: "202 Maple St",
+      address: "Cape Fear, NC",
     },
   ];
 
   return (
     <div className="property-container">
-      <h1>Top 5 Properties!</h1>
+      <h1>Today's Property Listings</h1>
       {topProperties.map((property) => (
         <div key={property.id} className="property-card">
           <img src={property.image} alt={`Property ${property.id}`} />
           <div className="property-details">
             <p>{property.price}</p>
             <p>{property.details}</p>
-            <p>{property.address}</p>
+            <p className="property-address">{property.address}</p>
+            <button className="green-button">Click for more</button>
           </div>
         </div>
       ))}
