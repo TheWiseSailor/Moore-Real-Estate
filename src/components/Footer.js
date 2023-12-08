c03af8d99d15def271d9315fef79c9edcd4ed687
TheWiseSailor <rudasilljay@gmail.com>
2023-12-07 22:12:03 -0500

    css styling with the footer and all links made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,51 @@
 import React from "react";
+import Instagram from "../components/SocialLinkImages/Instagram.png";
+import Facebook from "../components/SocialLinkImages/Facebook.png";
+import Twitter from "../components/SocialLinkImages/Twitter.png";
+
 import "../App.css";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-left">
         <div className="social-logos">
-          {/* Instagram Logo */}
-          <img
-            src="/path/to/instagram-logo.png"
-            alt="Instagram"
-            className="social-logo"
-          />
-
-          {/* Facebook Logo */}
-          <img
-            src="/path/to/facebook-logo.png"
-            alt="Facebook"
-            className="social-logo"
-          />
-
-          {/* Twitter Logo (latest version) */}
-          <img
-            src="/path/to/twitter-logo-x.png"
-            alt="Twitter"
-            className="social-logo"
-          />
+          {/* LOGOS ARE FROM FLATICON And ICONS8 */}
+          {/* Instagram Logo with direct link */}
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Instagram} alt="Instagram" className="social-logo" />
+          </a>
+          {/* Facebook Logo with link */}
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={Facebook}
+              alt="Facebook"
+              className="social-logo pl-6 pr-6"
+            />
+          </a>
+          {/* Twitter Logo (now called 'x') with link */}
+          <a
+            href="https://www.x.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={Twitter}
+              alt="Twitter (now called 'x')"
+              className="social-logo"
+            />
+          </a>
         </div>
       </div>
-      <div className="footer-right">
+      <div className="footer-right pt-4">
         <p>Copyright &copy; 2023 All rights reserved | Moore Real Estate</p>
       </div>
     </footer>
