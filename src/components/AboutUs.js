dad71b708291851bf1e29283937e0665b7216bb9
TheWiseSailor <rudasilljay@gmail.com>
2023-12-11 15:33:35 -0500

    minor aos changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,22 +1,24 @@
 import React from "react";
-import "../App.css"; // Make sure to adjust the path based on your project structure
-
+import "../App.css";
+import AOSInitializer from "./AOS/AOSInitializer";
 const AboutUs = () => {
   return (
     <div className="about-us-container">
-      <div className="about-us-content">
+      <AOSInitializer />
+
+      <div
+        className="about-us-content"
+        data-aos="fade-down"
+        data-aos-duration="3000"
+      >
+        <AOSInitializer />
         <div className="about-us-text pb-24">
           <h2>About Us</h2>
         </div>
         <div className="about-us-images d-flex ">
-          <div
-            className="about-us-image with-text-on-right"
-            data-aos="fade-down"
-            data-aos-duration="3000"
-          >
-            {/* Image on the left with text on the right */}
+          <div className="about-us-image with-text-on-right">
             <img
-              src="https://placekitten.com/400/300" // Replace with your image URL
+              src="https://placekitten.com/400/300"
               alt="Abstract Image Left"
             />
             <p className="AboutUsOne">
@@ -25,19 +27,14 @@ const AboutUs = () => {
               blandit volutpat nulla a mattis.
             </p>
           </div>
-          <div
-            className="about-us-image with-text-on-left pt-96"
-            data-aos="fade-up"
-            data-aos-duration="3000"
-          >
-            {/* Image on the right with text on the left */}
+          <div className="about-us-image with-text-on-left pt-96">
             <p className="AboutUsTwo">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
               efficitur urna at est dapibus, a lacinia lorem viverra. Integer
               blandit volutpat nulla a mattis.
             </p>
             <img
-              src="https://placekitten.com/400/300" // Replace with your image URL
+              src="https://placekitten.com/400/300"
               alt="Abstract Image Right"
             />
           </div>
