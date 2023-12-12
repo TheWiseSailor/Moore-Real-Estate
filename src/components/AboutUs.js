0a69fa6decd347746eb826d0d6aca4569b09d879
TheWiseSailor <rudasilljay@gmail.com>
2023-12-11 20:27:10 -0500

    minor changes willl do css changes when ever I get the chance

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import AOSInitializer from "./AOS/AOSInitializer";
+import AboutUsImage from "../components/images/AboutUsImages/About us.png";
+import TopAboutImage from "../components/images/AboutUsImages/TopAboutImage.png";
+import BottomAboutImage from "../components/images/AboutUsImages/BottomAboutImage.png";
 
 const AboutUs = () => {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 768);
+    };
+
+    // Set initial view
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="about-us-container">
       <AOSInitializer />
@@ -13,8 +35,24 @@ const AboutUs = () => {
         data-aos-duration="3000"
       >
         <AOSInitializer />
-        <div className="about-us-text ">
-          <h2 className="">About Us</h2>
+        <div className="about-us-text">
+          <h2>About Us</h2>
+          {isMobileView ? (
+            <>
+              <img
+                src={TopAboutImage}
+                alt="Top About Us"
+                className="about-us-image"
+              />
+              <img
+                src={BottomAboutImage}
+                alt="Bottom About Us"
+                className="about-us-image"
+              />
+            </>
+          ) : (
+            <img src={AboutUsImage} alt="About Us" className="about-us-image" />
+          )}
         </div>
       </div>
     </div>
