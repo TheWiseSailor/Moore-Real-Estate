f761f7ade305f2f0e843e1aaed8cf8b2b4f1bd15
TheWiseSailor <rudasilljay@gmail.com>
2023-12-11 20:19:22 -0500

    coded everything out and then took a ss of it because i would need more packages to us. and I shouldve done this on microsoft powerpoint but oh well. I had thing to take care of today so this saved me more time

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "../App.css";
 import AOSInitializer from "./AOS/AOSInitializer";
-import AboutImage1 from "../components/images/AboutUsImages/About1.png";
-import AboutImage2 from "../components/images/AboutUsImages/About2.png";
 
 const AboutUs = () => {
   return (
@@ -17,25 +15,6 @@ const AboutUs = () => {
         <AOSInitializer />
         <div className="about-us-text ">
           <h2 className="">About Us</h2>
-        </div>
-        <div className="about-us-images d-flex ">
-          <div className="about-us-image with-text-on-right">
-            {/* <a href="https://www.vecteezy.com/free-png/real-estate">Real Estate PNGs by Vecteezy</a> */}
-            <img src={AboutImage1} alt="AboutImage1" className="AboutImage1" />
-            <p className="AboutUsOne">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              efficitur urna at est dapibus, a lacinia lorem viverra. Integer
-              blandit volutpat nulla a mattis.
-            </p>
-          </div>
-          <div className="about-us-image with-text-on-left ">
-            <p className="AboutUsTwo">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              efficitur urna at est dapibus, a lacinia lorem viverra. Integer
-              blandit volutpat nulla a mattis.
-            </p>
-            <img src={AboutImage2} alt="AboutImage2" className="AboutImage2" />
-          </div>
         </div>
       </div>
     </div>
