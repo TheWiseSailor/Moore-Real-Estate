eb156fe96585007c2df3e1c66e5a7d6c6885d794
TheWiseSailor <rudasilljay@gmail.com>
2023-12-18 20:33:40 -0500

    all of the others work as well

--- src/components/Agents/AgentProfile.js
+++ src/components/Agents/AgentProfile.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import agent1Image from "../images/AgentHomepageImages/SamanthaBennett.png";
+import agent2Image from "../images/AgentHomepageImages/AnnaMae.png";
+import agent3Image from "../images/AgentHomepageImages/ZachHolland.png";
+import agent4Image from "../images/AgentHomepageImages/KenLowell.png";
 
 const agentData = [
   {
     id: 1,
     name: "Samantha Bennett",
     image: agent1Image,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    name: "Anna Mae",
+    image: agent2Image,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 3,
+    name: "Zach Holland",
+    image: agent3Image,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 4,
+    name: "Ken Lowell",
+    image: agent4Image,
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
 ];
